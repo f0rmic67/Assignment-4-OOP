baae990d45e04c29fc476cc7bf0fa00b5e82cedf
f0rmic67 <91424386+f0rmic67@users.noreply.github.com>
2021-11-23 11:58:32 -0500

    Final, Completed File

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ int main() {
 	{
 		cout << "Please enter the number of your desired action.\n" << endl;
 		cout << "1. Set the time\n2. Display the current time\n3. Advance one hour\n4. Advance one minute\n5. Add specific number of minutes\n6. Compare two times \n7. Quit" << endl;
-		cout << "\nNumber: ";
+		cout << "Number: ";
 	
 		while(!(cin >> choice) || cin.peek() != '\n' || choice < 1 || choice > 7)
    		{	
@@ -24,19 +24,15 @@ int main() {
     		cin.ignore(30000, '\n');            
    		}
    		
-   		cin.ignore();   //Without this ignore, getline in settime function will run without user input
+   		cin.ignore();   
    		
    		if(choice == 1)
    		{
-   			//cout << "\nPlease enter a time in the following format: 3:42 PM\nTime: ";
-   			//cin >> grandfather;
-   			
-   			grandfather.settime();
+   			cin >> grandfather;
    		}
    			
    		else if(choice == 2)
-   			//cout << grandfather;
-   			grandfather.display();
+   			cout << grandfather;
    			
    		else if(choice == 3)
    			grandfather.addhour();
@@ -60,18 +56,14 @@ int main() {
 	    
 	    else if(choice == 6)
 	    {
-	    	//cout << "\nEnter the time to be compared to the saved clock in the following format: 3:42 PM\nTime: ";
-	    	
-	    	//cin >> wall;
-	    	
-	    	wall.settime();
+	    	cin >> wall;
 	    	
 	    	do
 			{
 	    		cout << "\nHow would you like to compare the times?\n\n";
 	    		cout << "1. saved > new \n2. saved < new \n3. saved == new \n4. saved >= new\n5. saved <= new \n6. saved != new \n7. exit compare \n\n";
 	    	
-	    		cout << "Number: ";
+	    		cout << "Choice: ";
 	    	
 	    		while(!(cin >> compnum) || cin.peek() != '\n' || choice < 1 || choice > 7)
    				{	
@@ -84,42 +76,60 @@ int main() {
    				{
    					compare = grandfather > wall;
    				
-   					cout << "\nsaved > new: " << compare << endl;
+   					cout << "\nsaved > new: ";
+					wall.printbool(compare);
+					
+					cout << "\n";
 		    	}
 		    
 		  	   else if(compnum == 2)
 		 	    {
 		  	  		compare = grandfather < wall;
 		    	
-		  	 	 	cout << "\nsaved < new: " << compare << endl;
+		  	 	 	cout << "\nsaved < new: ";
+		  	 	 	wall.printbool(compare);
+		  	 	 	
+		  	 	 	cout << "\n";
 				}
 			
 				else if(compnum == 3)
 				{
 					compare = grandfather == wall;
 				
-					cout << "\nsaved == new: " << compare << endl;
+					cout << "\nsaved == new: ";
+					wall.printbool(compare);
+					
+					cout << "\n";
 				}
 			
 				else if(compnum == 4)
 				{
 					compare = grandfather >= wall;
 				
-					cout << "\nsaved >= new: " << compare << endl;
+					cout << "\nsaved >= new: ";
+					wall.printbool(compare);
+					
+					cout << "\n";
 				}
 			
 				else if(compnum == 5)
 				{
 					compare = grandfather <= wall;
 				
-					cout << "\nsaved <= new: " << compare << endl;
+					cout << "\nsaved <= new: ";
+					wall.printbool(compare);
+					
+					cout << "\n";
 				}
 			
 				else if(compnum == 6)
 				{
 					compare = grandfather != wall;
 				
-					cout << "\nsaved != new: " << compare << endl;
+					cout << "\nsaved != new: ";
+					wall.printbool(compare);
+					
+					cout << "\n";
 				}	
 			
 				else if(compnum == 7)
