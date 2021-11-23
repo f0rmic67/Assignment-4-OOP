bb65603f64129e72aedb45cdd821af7011c9506f
f0rmic67 <91424386+f0rmic67@users.noreply.github.com>
2021-11-23 11:57:11 -0500

    Final, Completed File

--- Clock.cpp
+++ Clock.cpp
@@ -5,12 +5,11 @@
 
 using namespace std;
 
-Clock::Clock()
+Clock::Clock()  //automatically sets clock time to midnight
 {
 	hour = 12;
 	minute = 00;
 	period = "AM";
-	badinput = false;
 	
 	return;
 }
@@ -31,13 +30,6 @@ void Clock::sethour(const int h)
 	return;
 }
 
-void Clock::setbadinput(const bool b)
-{
-	badinput = b;
-	
-	return;
-}
-
 void Clock::setminute(const int m)
 {
 	if(m >= 0 && m < 60)
@@ -46,86 +38,12 @@ void Clock::setminute(const int m)
 	return;
 }
 
-void Clock::settime()
-{
-	string time, hour1, minute1, period1;
-	int colonIndex, spaceIndex, digitCheck, hour2, minute2;
-	bool goodData = true;
-	
-	do
-	{	
-		cout << "\nPlease enter a time in the following format: 3:42 PM" << endl;
-		cout << "Time: ";
-		getline(cin, time);
-			
-		colonIndex = time.rfind(':');
-		spaceIndex = time.rfind(' ');
-		
-		if(colonIndex != -1 && spaceIndex != -1)
-		{
-			hour1 = time.substr(0, colonIndex);
-			minute1 = time.substr(colonIndex + 1, 2);
-			period1 = time.substr(spaceIndex + 1, 2);
-			
-			goodData = true;
-		}
-		else
-			goodData = false;
-		
-		if(goodData == true)		
-		{
-			for(int count = 0; count < hour1.length(); count++)
-			{
-				digitCheck = isdigit(hour1[count]);
-				
-				if(digitCheck == 0)
-					goodData = false;
-			} 
-		
-			for(int count = 0; count < minute1.length(); count++)
-			{
-				digitCheck = isdigit(minute1[count]);
-			
-				if(digitCheck == 0)
-					goodData = false;
-			} 
-			
-			if(goodData == true)
-			{
-				
-				hour2 = atoi(hour1.c_str());
-				
-				if(hour2 < 1 || hour2 > 12)
-					goodData = false;
-					
-				minute2 = atoi(minute1.c_str());
-				
-				if(minute2 < 0 || minute2 > 59)
-					goodData = false;
-			}
-			
-			if(period1 != "AM" && period1 != "PM")
-				goodData = false;   
-		}
-		
-		if(goodData == false)	
-			cout << "\nIncorrect format, please enter time again." << endl;
-	}
-	while(goodData == false);
-	
-	sethour(hour2);
-	setminute(minute2);
-	setperiod(period1);
-	
-	return;
-}
-
 void Clock::addhour()
 {
-	if(hour == 12)
+	if(hour == 12)  //ensures clock hour never exits the bounds of 1-12
 		hour = 1;
 		
-	else if(hour == 11)
+	else if(hour == 11)  //changes the period if necessary
 	{
 		if(period == "AM")
 		{
@@ -145,11 +63,20 @@ void Clock::addhour()
 	return;
 }
 
+void Clock::printbool(bool b1) const  //used to display relational operator results
+{
+	if(b1 == true)
+		cout << "True";
+	
+	else
+		cout << "False";
+}
+
 void Clock::addminute()
 {
-	if(minute == 59)
+	if(minute == 59)   //checks if adding one minute will also adjust the hour
 	{
-		if(hour == 11)
+		if(hour == 11)   //if hour is 11 and minutes are 59, increments and adjusts period
 		{
 			if(period == "AM")
 			{
@@ -165,20 +92,20 @@ void Clock::addminute()
 			}
 		}
 		
-		else if(hour == 12)
+		else if(hour == 12)  //rolls back to 1:00 when time is 12:59
 		{
 			minute = 0;
 			hour = 1;
 		}
 		
-		else
+		else  //changes hour when minutes = 59
 		{
 			hour++;
 			minute = 0;
 		}
 	}
 	
-	else
+	else    //adds one minute when no other adjustments are needed
 		minute++;
 	
 }
@@ -198,25 +125,6 @@ int Clock::getminute() const
 	return minute;
 }
 
-bool Clock::getbadinput() const
-{
-	return badinput;	
-}
-
-void Clock::display() const
-{
-	cout << "\n" << hour << ":";
-	
-	if(minute < 10)
-		cout << "0" << minute;
-	else
-		cout << minute;
-		
-	cout << " " << period << endl;
-	
-	return;
-}
-
 Clock operator+(const Clock& c1, const int& n1)  //overloaded to add specific number of minutes to time
 {
 	Clock answer;
@@ -226,7 +134,7 @@ Clock operator+(const Clock& c1, const int& n1)  //overloaded to add specific nu
 	
 	if(n1 > 0)
 	{
-		for(int count = 0; count < n1; count++)
+		for(int count = 0; count < n1; count++)  //calls the addminute function for every minute the user wants to add
 		{
 			answer.addminute();
 		}	
@@ -239,13 +147,12 @@ bool operator>(const Clock& c1, const Clock& c2)
 {
 	bool answer;
 	
-	if((c1.hour > c2.hour && c1.period == c2.period) || (c1.period == "PM" && c2.period == "AM"))
+	if((c1.hour > c2.hour && c1.period == c2.period) || (c1.period == "PM" && c2.period == "AM"))  //checks hours in the same period and hours in different periods
 	{
 		answer = true;
-		return answer;
 	}
 	
-	else if(c1.hour == c2.hour && c1.period == c2.period)
+	else if(c1.hour == c2.hour && c1.period == c2.period)  //checks times with the same hour + period
 	{
 		if(c1.minute > c2.minute)
 		{
@@ -258,7 +165,6 @@ bool operator>(const Clock& c1, const Clock& c2)
 	else
 	{
 		answer = false;
-		return false;
 	}
 	
 	return answer;
@@ -269,7 +175,7 @@ bool operator<(const Clock& c1, const Clock& c2)
 {
 	bool answer;
 	
-	if(operator>(c1, c2) == false && operator==(c1, c2) == false)
+	if(operator>(c1, c2) == false && operator==(c1, c2) == false)  //uses > and == operators to determine answer
 		answer = true;
 		
 	else
@@ -295,7 +201,7 @@ bool operator>=(const Clock& c1, const Clock& c2)
 {
 	bool answer;
 	
-	if(operator>(c1, c2) || operator==(c1, c2))
+	if(operator>(c1, c2) || operator==(c1, c2))  //combines > and == operators
 		answer = true;
 		
 	else
@@ -308,7 +214,7 @@ bool operator<=(const Clock& c1, const Clock& c2)
 {
 	bool answer;
 	
-	if(operator<(c1, c2) || operator==(c1, c2))
+	if(operator<(c1, c2) || operator==(c1, c2))  //combines < and == operators
 		answer = true;
 	
 	else
@@ -318,11 +224,11 @@ bool operator<=(const Clock& c1, const Clock& c2)
 
 }
 
-bool operator!=(const Clock& c1, const Clock& c2)
+bool operator!=(const Clock& c1, const Clock& c2) 
 {
 	bool answer;
 	
-	if(!(operator==(c1, c2)))
+	if(!(operator==(c1, c2)))  //uses == operator
 		answer = true;
 	
 	else
@@ -330,12 +236,12 @@ bool operator!=(const Clock& c1, const Clock& c2)
 		
 	return answer;
 }
-/*
+
 ostream& operator<<(ostream& out, const Clock& c1)
 {
 	out << "\n" << c1.hour << ":";
 	
-	if(c1.minute < 10)
+	if(c1.minute < 10)     //adds zero before single-digit minutes in output
 		out << "0" << c1.minute;
 	else
 		out << c1.minute;
@@ -347,12 +253,74 @@ ostream& operator<<(ostream& out, const Clock& c1)
 
 istream& operator>>(istream& inp, Clock& c1)
 {
-	string temp;
+	string time, hour1, minute1, period1;
+	int colonIndex, spaceIndex, digitCheck, hour2, minute2;
+	bool goodData = true;
 	
-	getline(inp, temp);
+	do
+	{	
+		cout << "\nPlease enter a time in the following format: 3:42 PM" << endl;
+		cout << "Time: ";
+		getline(inp, time);
+			
+		colonIndex = time.rfind(':');  //locates time format markers
+		spaceIndex = time.rfind(' ');
+		
+		if(colonIndex != -1 && spaceIndex != -1) 
+		{
+			hour1 = time.substr(0, colonIndex);       //stores hour string
+			minute1 = time.substr(colonIndex + 1, 2); //stores minute string
+			period1 = time.substr(spaceIndex + 1, 2); //stores period string
+			
+			goodData = true;
+		}
+		else
+			goodData = false;
+		
+		if(goodData == true)		
+		{
+			for(int count = 0; count < hour1.length(); count++) //checks hour string for good data
+			{
+				digitCheck = isdigit(hour1[count]); 
+				
+				if(digitCheck == 0)
+					goodData = false;
+			} 
+		
+			for(int count = 0; count < minute1.length(); count++) //checks minute string
+			{
+				digitCheck = isdigit(minute1[count]);
+			
+				if(digitCheck == 0)
+					goodData = false;
+			} 
+			
+			if(goodData == true)  //converts string data to int so it can be stored in Clock
+			{
+				
+				hour2 = atoi(hour1.c_str());
+				
+				if(hour2 < 1 || hour2 > 12)
+					goodData = false;
+					
+				minute2 = atoi(minute1.c_str());
+				
+				if(minute2 < 0 || minute2 > 59)
+					goodData = false;
+			}
+			
+			if(period1 != "AM" && period1 != "PM") //checks period string
+				goodData = false;   
+		}
+		
+		if(goodData == false)	
+			cout << "\nIncorrect format, please enter time again." << endl;  //prompts the user to input new time in the case of bad data
+	}
+	while(goodData == false);
 	
-	c1.settime(temp);
+	c1.sethour(hour2);
+	c1.setminute(minute2);   //sets input data to Clock
+	c1.setperiod(period1);
 	
 	return inp;
 }
-*/
